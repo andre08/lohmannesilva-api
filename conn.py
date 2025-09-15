0a4965cf1a60b7b1f957f957ca8ecd092d9faf41
Andre Ulisses da Silva <andre_ulisses@hotmail.com>
2025-09-14 23:06:48 -0300

    modified:   conn.py 	modified:   requirements.txt 	renamed:    script.sql -> script_mysql.sql 	new file:   script_sql_server.sql

--- conn.py
+++ conn.py
@@ -1,9 +1,15 @@
-import mysql.connector
+#import mysql.connector
 
+#def Conectar():
+#    return mysql.connector.connect(
+#        host="localhost",
+#        user="root",
+#        password="123456",
+#        database="DBFEATURESTORE"
+#    )
+
+import pyodbc
+
+connection_string = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:dw-powerbi.database.windows.net,1433;Database=app-feature-store;Uid=adminsitrador;Pwd=Lohmann@Silva2023;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
 def Conectar():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="123456",
-        database="DBFEATURESTORE"
-    )
+    return pyodbc.connect(connection_string)
